5fcf83999ae84aaa9dd238e2e3ac424df2f0e717
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-08 20:07:04 +0530

    if by chance loading screen work successfully

--- app/recipes/loading.tsx
+++ app/recipes/loading.tsx
@@ -1,3 +1,11 @@
+import RecipeCardSkeleton from "@/components/RecipeCardSkeleton";
+
 export default function Loading() {
-  return null
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      {Array.from({ length: 6 }).map((_, i) => (
+        <RecipeCardSkeleton key={i} />
+      ))}
+    </div>
+  )
 }
