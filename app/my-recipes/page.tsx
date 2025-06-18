1bd7ca52145c656e8ebb367176babf86cc4a95ff
adityarwt1 <adityarawatnew2487@gmail.com>
2025-06-18 21:13:32 +0530

    error solved in the rcipe

--- app/my-recipes/page.tsx
+++ app/my-recipes/page.tsx
@@ -4,6 +4,12 @@ import MyRecipie from "@/components/MyRecipie";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  profileImage: string;
+}
 const Page = () => {
   const [email, setEmail] = useState("");
   const router = useRouter();
@@ -12,7 +18,12 @@ const Page = () => {
     try {
       const user = await getUserInfo();
 
-      setEmail(user.email);
+      if (!user) {
+        router.push("/login");
+        return;
+      }
+
+      setEmail((user as User).email);
     } catch (error) {
       console.log("error in my recipes", error);
       router.push("/login");
