1af34a9c03591ce06af0b418a99a3cfc33fdb447
adityarwt1 <adityarawatnew2487@gmail.com>
2025-06-18 21:39:05 +0530

    my recipie now serverside rndering

--- app/my-recipes/page.tsx
+++ app/my-recipes/page.tsx
@@ -1,35 +1,10 @@
-"use client";
 import { getUserInfo } from "@/action/my-action";
 import MyRecipie from "@/components/MyRecipie";
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 
-const Page = () => {
-  const [email, setEmail] = useState("");
-  const router = useRouter();
-
-  const fetchUserInfo = async () => {
-    try {
-      const user = await getUserInfo();
-
-      if (!user) {
-        router.push("/login");
-        return;
-      }
-
-      const { email } = user as { email: string };
-      console.log("email", email);
-      setEmail(email);
-    } catch (error) {
-      console.log("error in my recipes", error);
-      router.push("/login");
-    }
-  };
-
-  useEffect(() => {
-    fetchUserInfo();
-  }, []);
-
+const Page = async () => {
+  const user = await getUserInfo();
+  const { email } = user as { email: string };
   return (
     <div>
       <MyRecipie email={email} />
