3cd90654b77c8a3940c69cb17653a749a582dd81
adityarwt1 <adityarawatnew2487@gmail.com>
2025-06-19 17:29:46 +0530

    tried for the next js

--- services/fetch.ts
+++ services/fetch.ts
@@ -1,6 +1,17 @@
-"use client";
-
-export const fetchRecipeByAuthor = async (email: string) => {
-  const response = await fetch(`/api/fetchrecipie?author=${email}`);
-  return response;
-};
+"use server";
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://food-book-vert.vercel.app"
+    : "http://localhost:3000";
+export async function getRecipieByAuthor(email: string) {
+  try {
+    const response = await fetch(`/api/fetchrecipie?author=${email}`);
+    const { recipes } = await response.json();
+    return recipes;
+  } catch (error: any) {
+    console.log(
+      "error while fetching the recipie witht the author",
+      error.message
+    );
+  }
+}
