00b81cae01f3c4a746db5412eb8af0b7d4918f0e
adityarwt1 <adityarawatnew2487@gmail.com>
2025-06-19 16:45:04 +0530

    add recipied cookie error resoilved

--- app/add-recipe/page.tsx
+++ app/add-recipe/page.tsx
@@ -1,18 +1,26 @@
 import { getUserInfo } from "@/action/my-action";
 import AddRecipePage from "@/components/AddRecipePage";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import jwt from "jsonwebtoken";
 import React from "react";
 
 const page = async () => {
-  const user = await getUserInfo();
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value || null;
+  if (!token) {
+    redirect("/login");
+  }
 
-  if (!user) {
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET as string);
+  const { email } = decoded as { email: string };
+  if (!email) {
     redirect("/login");
   }
 
   return (
     <>
-      <AddRecipePage email={user?.email} />
+      <AddRecipePage email={email} />
     </>
   );
 };
