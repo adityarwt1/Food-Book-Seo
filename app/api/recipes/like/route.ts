4ed56b9d258945d39bb3c852f49d18a4f3f2e6b5
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-07-03 10:00:10 +0530

    finally done the part of the like and the dislike

--- app/api/recipes/like/route.ts
+++ app/api/recipes/like/route.ts
@@ -1,82 +1,91 @@
-import connectDB from "@/lib/db"
-import { Recipe } from "@/models"
-import { NextRequest, NextResponse } from "next/server"
+import connectDB from "@/lib/db";
+import { Recipe } from "@/models";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
 
-    try {
-        await connectDB()
+    const searchParams = req.nextUrl.searchParams;
 
-        const searchParams = req.nextUrl.searchParams
+    const username = searchParams.get("username");
 
-        const username = searchParams.get("username")
+    const recipieId = searchParams.get("recipieId");
 
-        const recipieId = searchParams.get("recipieId")
-
-        const recipie = await Recipe.findOneAndUpdate({
-            _id: recipieId
+    if (!username || !recipieId) {
+      return NextResponse.json({ message: "bad request" }, { status: 400 });
+    }
+    const recipie = await Recipe.findOneAndUpdate(
+      {
+        _id: recipieId,
+      },
+      {
+        $inc: {
+          likes: +1,
         },
-            {
-                $inc: {
-                    likes: +1
-                },
-                $push:{
-                    likedBy: username
-                }
-
-            }
-        )
-        if( !recipie){
-            console.log("unable to find the recipie")
-            return NextResponse.json({success: false , message: "Unable to find the recipie" }, {status: 404})
-        }
-
-        return NextResponse.json({recipie: recipie.likedBy}, {status: 200})
-    } catch (error) {
-        console.log("Failed to increse like", error)
-        return NextResponse.json({ success: false, message: "error to save like or internal server issue" }, { status: 500 })
+        $push: {
+          likedBy: username,
+        },
+      }
+    );
+    if (!recipie) {
+      console.log("unable to find the recipie");
+      return NextResponse.json(
+        { success: false, message: "Unable to find the recipie" },
+        { status: 404 }
+      );
     }
 
+    return NextResponse.json({ likes: recipie.likedBy }, { status: 200 });
+  } catch (error) {
+    console.log("Failed to increse like", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "error to save like or internal server issue",
+      },
+      { status: 500 }
+    );
+  }
 }
 
-export async function DELETE(req:NextRequest) {
+export async function DELETE(req: NextRequest) {
+  try {
+    await connectDB();
 
-    try {
-        await connectDB()
+    const searchParams = req.nextUrl.searchParams;
 
-        const searchParams = req.nextUrl.searchParams
+    const username = searchParams.get("username");
 
-        const username = searchParams.get("username")
+    const recipieId = searchParams.get("recipieId");
 
-        const recipieId = searchParams.get("recipieId")
-
-        const recipie = await Recipe.findOneAndUpdate({
-            _id: recipieId
+    const recipie = await Recipe.findOneAndUpdate(
+      {
+        _id: recipieId,
+      },
+      {
+        $inc: {
+          likes: -1,
         },
-            {
-                $inc: {
-                    likes: -1
-                },
-                $pull:{
-                    likedBy: username
-                }
-
-            }
-        )
-        if( !recipie){
-            console.log("unable to find the recipie")
-            return NextResponse.json({success: false , message: "Unable to find the recipie" }, {status: 404})
-        }
-
-        return NextResponse.json({recipie: recipie.likedBy}, {status: 200})
-
-    } catch (error) {
-        console.log("error while disliking the recipie", error)
-        return NextResponse.json({message: "Internal server issue"}, {status: 500})
-        
+        $pull: {
+          likedBy: username,
+        },
+      }
+    );
+    if (!recipie) {
+      console.log("unable to find the recipie");
+      return NextResponse.json(
+        { success: false, message: "Unable to find the recipie" },
+        { status: 404 }
+      );
     }
 
-
-    
-    
+    return NextResponse.json({ likes: recipie.likedBy }, { status: 200 });
+  } catch (error) {
+    console.log("error while disliking the recipie", error);
+    return NextResponse.json(
+      { message: "Internal server issue" },
+      { status: 500 }
+    );
+  }
 }
